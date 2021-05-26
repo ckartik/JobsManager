cf31775504187bb816df9bef78a13bf63c7067bd
Kartik Chopra <k5chopra@uwaterloo.ca>
2021-05-26 11:46:10 -0400

    Adds to stubbing

--- jobs/jobs.go
+++ jobs/jobs.go
@@ -1,5 +1,33 @@
 package jobs
-func New() {}
-func Start(){}
-func Stop(){}
-func Query(){}
+
+import "sync"
+
+type jobsManger struct {
+	jobsPool sync.Map
+}
+
+type JobsManager interface {
+	Start(string) (bool, int)
+	Stop(int) (bool, error)
+	Query(int) (string, error)
+}
+
+func NewJobsManager() JobsManager {
+	return jobsManger{}
+}
+
+func (jm jobsManager) Start(string) (bool, int) {
+	_ = jm
+	return true, 0
+
+}
+
+func (jm jobsManager) Stop(string) (bool, error) {
+	_ = jm
+	return true, nil
+}
+
+func (jm jobsManager) Query(string) (string, error) {
+	_ = jm
+	return "", nil
+}
