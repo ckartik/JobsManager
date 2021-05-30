9d991cf9b3589f314802717dec230df361f5543e
Kartik Chopra <k5chopra@uwaterloo.ca>
2021-05-30 00:01:33 -0400

    Adds basic structure to server

--- server.go
+++ server.go
@@ -7,22 +7,27 @@ import (
     "net/http"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/ckaritk/JobsManager/Jobs"
+	"github.com/ckaritk/JobsManager/Jobs" // TODO: @ckartik - change this to lowercase in final impl.
   )
 )
 
 type Env struct {
-	jm JobsManager
+	jm Jobs.JobsManager
 }
 
 func main() {
 	r := chi.NewRouter()
+
+	// Dependency Injection.
+	env := &Env{}
   
 	// A good base middleware stack
+	/*
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	*/
   
 	// Set a timeout value on the request context (ctx), that will signal
 	// through ctx.Done() that the request has timed out and further
@@ -35,60 +40,40 @@ func main() {
   
 	// RESTy routes for "Jobs" resource
 	r.Route("/job", func(r chi.Router) {
-	  r.Post("/", createJob)
-
+	  r.Post("/", env.createJob)
+	  /*
 	  // Subrouters:
 	  r.Route("/{jobID}", func(r chi.Router) {
-		r.Use(JobCtx)
-		r.Get("/stop", getArticle)
-		r.Get("/status", getArticle)
+		r.Use(env.JobsCtx)
 		r.Get("/", getArticle)
 	  })
+	  */
 	})
   
 	http.ListenAndServe(":8443", r)
-  }
-  
-  func JobCtx(next http.Handler) http.Handler {
+}
+
+func (env * Env) JobsCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	  articleID := chi.URLParam(r, "articleID")
-	  article, err := dbGetArticle(articleID)
-	  if err != nil {
-		http.Error(w, http.StatusText(404), 404)
-		return
-	  }
-	  ctx := context.WithValue(r.Context(), "article", article)
-	  next.ServeHTTP(w, r.WithContext(ctx))
-	})
-  }
-  
-  func getArticle(w http.ResponseWriter, r *http.Request) {
+		jobID := chi.URLParam(r, "jobID")
+		job, ok := env.jm.jobs.Load(jobID)
+		if !ok {
+		  http.Error(w, http.StatusText(422), 422)
+		  return
+		}
+		ctx := context.WithValue(r.Context(), "job", job)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	  })
+}
+
+
+// TODO: @ckartik add authed info to request context.
+ func (env *Env) createJob(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	article, ok := ctx.Value("article").(*Article)
+	article, ok := ctx.Value("job").(*exec.Cmd)
 	if !ok {
 	  http.Error(w, http.StatusText(422), 422)
 	  return
 	}
 	w.Write([]byte(fmt.Sprintf("title:%s", article.Title)))
-  }
-  
-  // A completely separate router for administrator routes
-  func adminRouter() http.Handler {
-	r := chi.NewRouter()
-	r.Use(AdminOnly)
-	r.Get("/", adminIndex)
-	r.Get("/accounts", adminListAccounts)
-	return r
-  }
-  
-  func AdminOnly(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	  ctx := r.Context()
-	  perm, ok := ctx.Value("acl.permission").(YourPermissionType)
-	  if !ok || !perm.IsAdmin() {
-		http.Error(w, http.StatusText(403), 403)
-		return
-	  }
-	  next.ServeHTTP(w, r)
-	})
   }
