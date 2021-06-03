38320ab8997b4766ff0eaadbce06135d94b90fa0
Kartik Chopra <k5chopra@uwaterloo.ca>
2021-06-03 12:02:22 -0400

    Adds cert loading

--- server.go
+++ server.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
@@ -52,8 +55,31 @@ func main() {
 		*/
 	})
 
+	// Create a CA certificate pool and add cert.pem to it
+	caCert, err := ioutil.ReadFile("cert.pem")
+	if err != nil {
+		log.Fatal(err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// Create the TLS Config with the CA pool and enable Client certificate validation
+	tlsConfig := &tls.Config{
+		ClientCAs:  caCertPool,
+		ClientAuth: tls.RequireAndVerifyClientCert,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	// Create a Server instance to listen on port 8443 with the TLS config
+	server := &http.Server{
+		Addr:      ":8443",
+		TLSConfig: tlsConfig,
+	}
+
 	log.Println("Starting server at 8443")
-	http.ListenAndServe(":8443", r)
+	log.Fatal(server.ListenAndServeTLS("cert.pem", "key.pem"))
+
+	// http.ListenAndServe(":8443", r)
 }
 
 /*
